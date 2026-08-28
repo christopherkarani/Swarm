2f4a0082a7381c2206b9bc346793bdfc03865fcd
christopherkarani <chrisbkarani@gmail.com>
2026-08-28 06:55:25 +0300

    refactor(workflow): remove durable run alias

--- Sources/Swarm/Workflow/Workflow+Durable.swift
+++ Sources/Swarm/Workflow/Workflow+Durable.swift
@@ -62,11 +62,6 @@ public extension Workflow {
             try await workflow.executeDurable(input, resumeFrom: checkpointID)
         }
 
-        /// Backward-compatible alias for `execute(_:resumeFrom:)`.
-        @available(*, deprecated, renamed: "execute(_:resumeFrom:)")
-        public func run(_ input: String, resumeFrom checkpointID: String? = nil) async throws -> AgentResult {
-            try await execute(input, resumeFrom: checkpointID)
-        }
     }
 }
 
